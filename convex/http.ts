1a4f88c91370cb7a244d34af52551ff96b540e4d
Aalaa-magdy <am7371494@gmail.com>
2025-11-18 20:57:23 +0200

    setup the convex for storing database and webhook of clerk

---
+++ convex/http.ts
@@ -0,0 +1,56 @@
+import { httpRouter } from "convex/server";
+import { httpAction } from "./_generated/server";
+import { Webhook } from "svix";
+import {WebhookEvent} from "@clerk/nextjs/server"
+import {api} from "./_generated/api"
+const http= httpRouter()
+const clerkWebhook = httpAction(async (ctx,request) => {
+    const webhookSecret = process.env.CLERK_WEBHOOK_SECRET;
+    if (!webhookSecret) {
+        throw new Error("CLERK_WEBHOOK_SECRET is not defined");
+    }
+    const svix_id = request.headers.get("svix-id");
+    const svix_timestamp = request.headers.get("svix-timestamp");
+    const svix_signature = request.headers.get("svix-signature");
+    if (!svix_id || !svix_timestamp || !svix_signature) {
+        return new Response("Missing Svix headers", { status: 400 });
+    }
+    const payload = await request.json();
+    const body = JSON.stringify(payload);
+
+    const wh = new Webhook(webhookSecret);
+    let evt: WebhookEvent;
+    try {
+        evt = wh.verify(body,
+            {"svix-id":svix_id,
+            "svix-timestamp":svix_timestamp,
+            "svix-signature":svix_signature}
+        ) as WebhookEvent }
+     catch(error){
+        console.error("Error verifying webhook:", error);
+        return new Response("Invalid signature", { status: 400 });
+     }
+      
+     const eventType = evt.type;
+     
+     if(eventType === "user.created"){
+      const {id,email_addresses,first_name,last_name} = evt.data;
+        const email = email_addresses.length > 0 ? email_addresses[0].email_address : "";
+        const name = `${first_name || ""} ${last_name || ""}`.trim();
+
+        try{
+           await ctx.runMutation(api.users.createUser,{clerkId:id,email,name});
+        } catch(error){
+              console.error("Error creating user in Convex:", error);
+              return new Response("Error creating user", { status: 500 });
+        }
+    } 
+    return new Response("Webhook processed Successfully", { status: 200 });
+}
+)
+http.route({
+    path:"/clerk-webhook",
+    method:"POST",
+    handler: clerkWebhook
+})
+export default http;
