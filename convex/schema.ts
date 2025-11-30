4d12d7fa22682a002110c8dc7f705085a4a65c06
Aalaa-magdy <am7371494@gmail.com>
2025-11-30 22:48:39 +0200

    Perform the Main page

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,5 +1,6 @@
 import {defineSchema, defineTable} from "convex/server";
 import {v} from "convex/values";
+import { title } from "process";
 
 export default defineSchema({
 
@@ -8,4 +9,11 @@ export default defineSchema({
         name: v.string(),
         clerkId : v.string(),
     }).index("by_clerkId", ["clerkId"]),
+
+    courses : defineTable({
+        title: v.string(),
+        description: v.string(),
+        price: v.number(),
+        imageUrl: v.string(),
+    }),
 });
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,83 @@
-"use client";
+
+
+import { ConvexHttpClient } from "convex/browser";
 import { api } from "../../convex/_generated/api";
-import { useQuery } from "convex/react";
-export default function Home() {
-  const tasks = useQuery(api.tasks.getAllTasks);
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import Link from "next/link";
+import Image from "next/image";
+import { Badge } from "@/components/ui/badge";
+import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { ArrowRight } from "lucide-react";
+
+export default async function Home() {
+  const convex = new ConvexHttpClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+  const courses = await convex.query(api.courses.getCourses);
   return (
      <>
-       <h1>All tasks in database</h1>
-        {tasks ?.map((task) => (
-          <div key={task._id}>
-            <h2>{task.text}</h2>
-            <p>Is Completed ?{task.isCompleted}</p>
-          </div>
-        ))}
+       <div className="flex flex-col min-h-screen">
+
+        <main className="flex-grow container mx-auto px-4 py-16">
+           <div className="text-center mb-16">
+              <h1 className="text-4xl font-extrabold mb-4">Forge Your Path in Modern Development</h1>
+              <p className="text-xl text-muted-foreground max-w-2xl mx-auto">
+                Master fullstack skills through hands-on projects and expert-led courses.
+              </p>
+           </div>
+
+           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mb-16">
+              {
+                courses.slice(0,3).map(course =>(
+                  <Card key={course._id} className="flex flex-col">
+                      <Link href={`/courses/${course._id}`} className="cursor-pointer" >
+                        <CardHeader>
+                            <Image 
+                              src={course.imageUrl}
+                              alt={course.title}
+                              width={400}
+                              height={200}
+                              className="rounded-md object-cover"
+                            />
+                        </CardHeader>
+                        <CardContent className='flex-grow'>
+                          <CardTitle className="text-xl mb-2 hover:underline">{course.title}</CardTitle>
+
+                        </CardContent>
+                       </Link>
+
+                       <CardFooter className="flex justify-between items-center" >
+                         <Badge variant="default" className="text-lg px-3 py-1">
+                            ${course.price.toFixed(2)}
+                         </Badge>
+                    
+                       <SignedIn>
+                          Enroll
+                       </SignedIn>
+                       <SignedOut>
+                          <SignInButton mode="modal">
+                             <Button variant={"outline"}>
+                                Enroll Now 
+                             </Button>
+                          </SignInButton>
+                       </SignedOut>
+                      </CardFooter>
+                  </Card>
+                )) 
+              }
+           </div>
+
+           <div className="text-center">
+              <Link href="/pro">
+                 <Button size={"lg"} className="group hover:bg-purple-600 transition-colors duration-300">
+                    Explore Pro Plans
+                    <ArrowRight className="ml-2 h-4 w-4 group-hover:translate-x-1 transition-transform" />
+                 </Button>
+              </Link>
+           </div>
+         </main>
+
+       </div>
+        
      </>
   );
 }
